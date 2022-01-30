c3a167b642a33b50adfdb6a313452c4918091ca8
Kloong1 <dkdlsmsm98@gmail.com>
2022-01-30 23:31:11 +0900

    baekjoon 2133 add comments. Need to try again next time.

--- baekjoon_2133/src/Main.java
+++ baekjoon_2133/src/Main.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/*
+점화식 자체가 복잡하다고 볼 수는 없지만
+경우의 수를 타일을 그려보며 하나하나 생각해야 하는 문제.
+구글링해서 천천히 생각해보고 다시 도전해볼 것.
+ */
+
 public class Main
 {
     public static void main(String[] args)
