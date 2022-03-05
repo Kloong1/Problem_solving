4b2664435a2d2d3f44f84d049b697381f7da535e
Kloong1 <dkdlsmsm98@gmail.com>
2022-03-05 23:00:29 +0900

    beakjoon 17472 need to modify

--- baekjoon_17472/src/Main.java
+++ baekjoon_17472/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main
@@ -16,37 +14,159 @@ public class Main
         map = new int[rows][cols];
         readMap();
 
-        ArrayList<Island> islands = findAllIslands();
+        int numOfIslands = findAllIslands();
 
-        for (int[] row : map)
-            System.out.println(Arrays.toString(row));
+        int[][] minBridgeLength = new int[numOfIslands + 1][numOfIslands + 1];
+        initMinBridgeLength(minBridgeLength);
+        getAllMinBridgeLength(minBridgeLength);
+
+        DisjointSet ds = new DisjointSet(numOfIslands);
+        int min = getSumOfMinBridgeLength(1, minBridgeLength, ds, numOfIslands, 0, 0);
+
+        if (min == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(min);
     }
 
-    static ArrayList<Island> findAllIslands()
+    static int getSumOfMinBridgeLength(int curIsland, int[][] minBridgeLength, DisjointSet ds, int numOfIslands, int bridgeCnt, int sum)
     {
-        ArrayList<Island> islands = new ArrayList<>();
+        if (curIsland > numOfIslands)
+        {
+            if (bridgeCnt != numOfIslands - 1)
+                return Integer.MAX_VALUE;
+            return sum;
+        }
 
+        int minSum;
+        minSum = Integer.MAX_VALUE;
+
+        minSum = Math.min(minSum, getSumOfMinBridgeLength(curIsland + 1, minBridgeLength, ds.copy(), numOfIslands, bridgeCnt, sum));
+
+        for (int targetIsland = 1; targetIsland <= numOfIslands; targetIsland++)
+        {
+            if (minBridgeLength[curIsland][targetIsland] != Integer.MAX_VALUE)
+            {
+                if (ds.find(curIsland) != ds.find(targetIsland))
+                {
+                    DisjointSet dsCopy = ds.copy();
+                    dsCopy.union(curIsland, targetIsland);
+                    minSum = Math.min(minSum,
+                            getSumOfMinBridgeLength(curIsland + 1, minBridgeLength, dsCopy, numOfIslands, bridgeCnt + 1, sum + minBridgeLength[curIsland][targetIsland]));
+                }
+            }
+        }
+
+        return minSum;
+    }
+
+    static void getAllMinBridgeLength(int[][] minBridgeLength)
+    {
+        getMinHorizontalBridgeLength(minBridgeLength);
+        getMinVerticalBridgeLength(minBridgeLength);
+    }
+
+    static void getMinVerticalBridgeLength(int[][] minBridgeLength)
+    {
+        int lastIsland;
+        boolean seaFlag;
+        int bridgeLength;
+
+        for (int col = 0; col < cols; col++)
+        {
+            lastIsland = map[0][col];
+            seaFlag = false;
+            bridgeLength = 0;
+
+            for (int row = 1; row < rows; row++)
+            {
+                if (map[row][col] == -1)
+                {
+                    seaFlag = true;
+                    if (lastIsland != -1)
+                        bridgeLength++;
+                }
+                else
+                {
+                    if (seaFlag && lastIsland != -1 && bridgeLength >= 2)
+                    {
+                        minBridgeLength[lastIsland][map[row][col]] =
+                                Math.min(minBridgeLength[lastIsland][map[row][col]], bridgeLength);
+                        minBridgeLength[map[row][col]][lastIsland] =
+                                Math.min(minBridgeLength[map[row][col]][lastIsland], bridgeLength);
+                    }
+
+                    seaFlag = false;
+                    lastIsland = map[row][col];
+                    bridgeLength = 0;
+                }
+            }
+        }
+    }
+
+    static void getMinHorizontalBridgeLength(int[][] minBridgeLength)
+    {
+        int lastIsland;
+        boolean seaFlag;
+        int bridgeLength;
+
+        for (int row = 0; row < rows; row++)
+        {
+            lastIsland = map[row][0];
+            seaFlag = false;
+            bridgeLength = 0;
+
+            for (int col = 1; col < cols; col++)
+            {
+                if (map[row][col] == -1)
+                {
+                    seaFlag = true;
+                    if (lastIsland != -1)
+                        bridgeLength++;
+                }
+                else
+                {
+                    if (seaFlag && lastIsland != -1)
+                    {
+                        minBridgeLength[lastIsland][map[row][col]] =
+                                Math.min(minBridgeLength[lastIsland][map[row][col]], bridgeLength);
+                        minBridgeLength[map[row][col]][lastIsland] =
+                                Math.min(minBridgeLength[map[row][col]][lastIsland], bridgeLength);
+                    }
+
+                    seaFlag = false;
+                    lastIsland = map[row][col];
+                    bridgeLength = 0;
+                }
+            }
+        }
+    }
+
+    static void initMinBridgeLength(int[][] minBridgeLength)
+    {
+        for (int[] row : minBridgeLength)
+            Arrays.fill(row, Integer.MAX_VALUE);
+    }
+
+    static int findAllIslands()
+    {
         boolean[][] visit = new boolean[rows][cols];
-        int mapId = 1;
+        int mapId = 0;
 
         for (int row = 0; row < rows; row++)
         {
             for (int col = 0; col < cols; col++)
             {
                 if (map[row][col] == 0 && !visit[row][col]) //바다 = -1, 땅 = 0
-                {
-                    ArrayList<Land> lands = findLandsOfIsland(row, col, mapId++, visit);
-                    islands.add(new Island(lands));
-                }
+                    findLandsOfIsland(row, col, ++mapId, visit);
             }
         }
 
-        return islands;
+        return mapId;
     }
 
-    static ArrayList<Land> findLandsOfIsland(int startRow, int startCol, int mapId, boolean[][] visit)
+    static void findLandsOfIsland(int startRow, int startCol, int mapId, boolean[][] visit)
     {
-        ArrayList<Land> lands = new ArrayList<>();
         int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         Queue<Land> bfsQ = new LinkedList<>();
@@ -60,7 +180,6 @@ public class Main
         {
             land = bfsQ.poll();
             map[land.row][land.col] = mapId;
-            lands.add(land);
 
             for (int[] dir : dirs)
             {
@@ -77,8 +196,6 @@ public class Main
                 bfsQ.add(new Land(targetRow, targetCol));
             }
         }
-
-        return lands;
     }
 
     static boolean isLand(int row, int col)
@@ -127,5 +244,41 @@ class Land
     {
         this.row = row;
         this.col = col;
+    }
+}
+
+class DisjointSet
+{
+    int[] parent;
+
+    DisjointSet() {}
+
+    DisjointSet(int numOfElements)
+    {
+        parent = new int[numOfElements + 1];
+        for (int i = 0; i < parent.length; i++)
+            parent[i] = i;
+    }
+
+    int find(int e)
+    {
+        if (parent[e] == e)
+            return e;
+        parent[e] = find(parent[e]);
+        return parent[e];
+    }
+
+    void union(int e1, int e2)
+    {
+        int pe1 = find(e1);
+        int pe2 = find(e2);
+        parent[pe2] = pe1;
+    }
+
+    DisjointSet copy()
+    {
+        DisjointSet ds = new DisjointSet();
+        ds.parent = Arrays.copyOf(this.parent, parent.length);
+        return ds;
     }
 }
