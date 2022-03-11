270bd3026e854d8b49ba44024660cd4970bea151
Kloong1 <dkdlsmsm98@gmail.com>
2022-03-11 18:27:11 +0900

    baekjoon 1937 refactoring

--- baekjoon_1937/src/Main.java
+++ baekjoon_1937/src/Main.java
@@ -1,3 +1,5 @@
+/* https://kloong.tistory.com/entry/욕심쟁이-판다-1937-Java */
+
 import java.io.*;
 import java.util.*;
 
@@ -5,8 +7,8 @@ public class Main
 {
     static int N;
     static int[][] bamboos;
-    static int[][] maxMovesArr;
-    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int[][] maxMovesArr; //DP 배열
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; //상하좌우 이동에 쓰일 배열
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -24,44 +26,52 @@ public class Main
     {
         int maxMoves = 0;
 
+        //DP 배열 생성 및 0으로 초기화
         maxMovesArr = new int[N][N];
 
         for (int row = 0; row < N; row++)
         {
             for (int col = 0; col < N; col++)
-                maxMoves = Math.max(maxMoves, dfs2GetMaxMoves(row, col));
+                maxMoves = Math.max(maxMoves, dfs2GetMaxMoves(row, col)); //모든 칸에서 DFS
         }
 
         return maxMoves;
     }
 
-    static int dfs2GetMaxMoves(int startRow, int startCol)
+    //DFS를 하며 최댓값을 구하고 기록하는 함수
+    static int dfs2GetMaxMoves(int row, int col)
     {
-        if (maxMovesArr[startRow][startCol] != 0)
-            return maxMovesArr[startRow][startCol];
+        //이미 DP 배열에 값이 있으면 바로 return
+        if (maxMovesArr[row][col] != 0)
+            return maxMovesArr[row][col];
 
+        //출발하는 칸을 포함해야 하기 때문에 1부터 시작
         int maxMoves = 1;
 
+        //상하좌우로 움직이며 DFS가 가능할 경우 DFS
         int nextRow, nextCol;
         for (int[] dir : dirs)
         {
-            nextRow = startRow + dir[0];
-            nextCol = startCol + dir[1];
+            nextRow = row + dir[0];
+            nextCol = col + dir[1];
 
             if (!isInMap(nextRow, nextCol))
                 continue;
 
-            if (bamboos[nextRow][nextCol] <= bamboos[startRow][startCol])
+            if (bamboos[nextRow][nextCol] <= bamboos[row][col])
                 continue;
 
+            //해당 칸으로 한 칸 움직였으므로 + 1 한 값과 비교해야함
             maxMoves = Math.max(maxMoves, dfs2GetMaxMoves(nextRow, nextCol) + 1);
         }
 
-        maxMovesArr[startRow][startCol] = maxMoves;
+        //DP 배열에 기록
+        maxMovesArr[row][col] = maxMoves;
 
         return maxMoves;
     }
 
+    //해당 row, col이 범위 내의 값인지 확인
     static boolean isInMap(int row, int col)
     {
         return row >= 0 && row < N && col >= 0 && col < N;
