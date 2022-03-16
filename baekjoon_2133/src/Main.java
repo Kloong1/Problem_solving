9d984935d4b0de3b278b9a327afc7a4d35830b95
Kloong1 <dkdlsmsm98@gmail.com>
2022-03-16 21:06:11 +0900

    baekjoon 2133

--- baekjoon_2133/src/Main.java
+++ baekjoon_2133/src/Main.java
@@ -1,11 +1,5 @@
 import java.util.Scanner;
 
-/*
-점화식 자체가 복잡하다고 볼 수는 없지만
-경우의 수를 타일을 그려보며 하나하나 생각해야 하는 문제.
-구글링해서 천천히 생각해보고 다시 도전해볼 것.
- */
-
 public class Main
 {
     public static void main(String[] args)
@@ -14,18 +8,29 @@ public class Main
 
         int wallWidth = sc.nextInt();
 
-        System.out.println(countTileCase(wallWidth));
+        System.out.println(getAnswer(wallWidth));
     }
 
-    static int countTileCase(int wallWidth)
+    static long getAnswer(int wallWidth)
     {
         if (wallWidth % 2 == 1)
             return 0;
 
-        int cnt = 0;
+        long[] dp = new long[wallWidth + 1];
+        dp[2] = 3;
 
+        for (int width = 4; width <= wallWidth; width += 2)
+        {
+            dp[width] += dp[width - 2] * 3;
 
+            //idx 너비를 제외하고 나머지를 특이 모양으로 채우는 경우
+            for (int idx = 4; width - idx >= 2; idx += 2)
+                dp[width] += dp[width - idx] * 2;
 
-        return cnt;
+            //특이 모양으로 width만큼 전부 채우는 경우
+            dp[width] += 2;
+        }
+
+        return dp[wallWidth];
     }
 }
