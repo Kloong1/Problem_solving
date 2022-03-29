ee438567c6aad38050f5230bed9156782f2b7676
Kloong1 <dkdlsmsm98@gmail.com>
2022-03-29 21:04:34 +0900

    merge sort refactoring

--- sorting_algorithm/merge_sort/src/MergeSort.java
+++ sorting_algorithm/merge_sort/src/MergeSort.java
@@ -33,13 +33,13 @@ public class MergeSort
 
     private static void mergeSort(int[] arr, int left, int right)
     {
-        if (left < right)
-        {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
-        }
+        if (left >= right)
+            return;
+
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
     }
 
     private static void merge(int[] arr, int left, int mid, int right)
