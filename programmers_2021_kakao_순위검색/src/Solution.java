6f2f02fa02f2587b06335aa633ab24556c91f265
Kloong1 <dkdlsmsm98@gmail.com>
2022-05-13 16:34:31 +0900

    programmers 2021 kakao 순위 검색

--- programmers_2021_kakao_순위검색/src/Solution.java
+++ programmers_2021_kakao_순위검색/src/Solution.java
@@ -3,114 +3,102 @@ import java.util.Collections;
 import java.util.HashMap;
 
 class Solution {
-    public int[] solution(String[] info, String[] query) {
-        QueryTree queryTree = new QueryTree();
-        queryTree.storeInfo(info);
-
-        int[] answer = new int[query.length];
-        for (int i = 0; i < query.length; i++)
-            answer[i] = queryTree.query(query[i]);
-
-        return answer;
-    }
-}
-
-class QueryTree {
     private static final String[][] CATEGORIES = new String[][]
             {{"cpp", "java", "python"}, {"backend", "frontend"}, {"junior", "senior"}, {"chicken", "pizza"}};
 
-    private final Node root;
+    private static final HashMap<String, ArrayList<Integer>> scoreListHashMap = new HashMap<>();
 
-    public QueryTree() {
-        this.root = new Node(null);
-        initQueryTree(root, 0);
+    public int[] solution(String[] info, String[] queryArr) {
+        initScoreListHashMap();
+
+        storeInfoToScoreListHashMap(info);
+
+        int[] answer = new int[queryArr.length];
+        for (int i = 0; i < queryArr.length; i++)
+            answer[i] = processQuery(queryArr[i]);
+
+        return answer;
     }
 
-    private void initQueryTree(Node node, int categoryIdx) {
-        if (categoryIdx == CATEGORIES.length) {
-            node.scoreList = new ArrayList<>(10_000);
-            node.isLeaf = true;
-            return;
-        }
+    private int processQuery(String query) {
+        String[] keysAndScore = query.split(" ");
 
-        for (String key : CATEGORIES[categoryIdx]) {
-            Node childNode = new Node(key);
-            node.childMap.put(key, childNode);
-            initQueryTree(childNode, categoryIdx + 1);
-        }
+        int score = Integer.parseInt(keysAndScore[keysAndScore.length - 1]);
+
+        String[] keys = new String[CATEGORIES.length];
+        for (int i = 0; i < keys.length; i++)
+            keys[i] = keysAndScore[i * 2];
+
+        return dfsToCountScores(keys, score, 0, "");
     }
 
-    public void storeInfo(String[] infoStrings) {
-        for (String infoString : infoStrings)
-            storeInfo(infoString);
-
-        sortAllScoreList(root);
-    }
-
-    private void storeInfo(String infoString) {
-        String[] keys = infoString.split(" ");
-
-        Node node = root;
-        int idx = 0;
-        while (!node.isLeaf) {
-            node = node.childMap.get(keys[idx]);
-            idx++;
-        }
-
-        node.scoreList.add(Integer.parseInt(keys[idx]));
-    }
-
-    private void sortAllScoreList(Node node) {
-        if (!node.isLeaf) {
-            for (Node childNode : node.childMap.values())
-                sortAllScoreList(childNode);
-        }
-        else {
-            Collections.sort(node.scoreList);
-        }
-    }
-
-    public int query(String queryString) {
-        String[] words = queryString.split(" ");
-
-        String[] keys = new String[4];
-
-        for (int i = 0; i <= 3; i++)
-            keys[i] = words[i * 2];
-
-        int score = Integer.parseInt(words[7]);
-
-        return getAnswer(root, score, keys, 0);
-    }
-
-    private int getAnswer(Node node, int score, String[] keys, int keyIdx) {
-        if (node.isLeaf) {
-            int idx = Collections.binarySearch(node.scoreList, score);
-            if (idx < 0) {
-                idx = -(idx + 1);
-            }
-            return node.scoreList.size() - idx;
+    private int dfsToCountScores(String[] keys, int score, int keyIdx, String keyString) {
+        if (keyIdx == keys.length) {
+            ArrayList<Integer> scoreList = scoreListHashMap.get(keyString);
+            int idx = lowerBound(scoreList, score);
+            return scoreList.size() - idx;
         }
 
         if (keys[keyIdx].equals("-")) {
             int cnt = 0;
-            for (Node childNode : node.childMap.values())
-                cnt += getAnswer(childNode, score, keys, keyIdx + 1);
+            for (String key : CATEGORIES[keyIdx])
+                cnt += dfsToCountScores(keys, score, keyIdx + 1, keyString + key);
             return cnt;
         }
 
-        Node childNode = node.childMap.get(keys[keyIdx]);
-        return getAnswer(childNode, score, keys, keyIdx + 1);
+        return dfsToCountScores(keys, score, keyIdx + 1, keyString + keys[keyIdx]);
     }
-}
 
-class Node {
-    String key;
-    HashMap<String, Node> childMap = new HashMap<>(10);
-    ArrayList<Integer> scoreList = null;
-    boolean isLeaf = false;
+    private int lowerBound(ArrayList<Integer> list, int target) {
+        int left, right, mid;
 
-    public Node(String key) {
-        this.key = key;
+        left = 0;
+        right = list.size();
+
+        while (left < right) {
+            mid = (left + right) / 2;
+            if (target > list.get(mid))
+                left = mid + 1;
+            else
+                right = mid;
+        }
+
+        return right;
+    }
+
+    private void storeInfoToScoreListHashMap(String[] infoArr) {
+        for (String info : infoArr) {
+            String[] keysAndScore = info.split(" ");
+
+            StringBuilder keyStringBuilder = new StringBuilder();
+            for (int i = 0; i < keysAndScore.length - 1; i++)
+                keyStringBuilder.append(keysAndScore[i]);
+
+            String key = keyStringBuilder.toString();
+            int score = Integer.parseInt(keysAndScore[keysAndScore.length - 1]);
+
+            scoreListHashMap.get(key).add(score);
+        }
+
+        for (ArrayList<Integer> scoreList : scoreListHashMap.values())
+            Collections.sort(scoreList);
+    }
+
+    private void initScoreListHashMap() {
+        ArrayList<String> keyArrayList = new ArrayList<>(24);
+        generateKeyArrayList(keyArrayList, "", 0);
+
+        for (String key : keyArrayList)
+            scoreListHashMap.put(key, new ArrayList<Integer>());
+    }
+
+    private void generateKeyArrayList(ArrayList<String> keyArrayList, String keyString, int categoryIdx) {
+        if (categoryIdx == CATEGORIES.length) {
+            keyArrayList.add(keyString);
+            return;
+        }
+
+        for (String key : CATEGORIES[categoryIdx])
+            generateKeyArrayList(keyArrayList, keyString + key, categoryIdx + 1);
     }
 }
