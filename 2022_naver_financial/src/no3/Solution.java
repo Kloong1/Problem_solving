3e6f3d3daca2e85232d2a9b0699813113b897323
Kloong1 <dkdlsmsm98@gmail.com>
2022-10-28 22:56:01 +0900

    2022 naver financial coding test

--- 2022_naver_financial/src/no3/Solution.java
+++ 2022_naver_financial/src/no3/Solution.java
@@ -70,12 +70,4 @@ class Solution {
 
         return answer;
     }
-
-    public <T> List<T> twoDArrayToList(T[][] twoDArray) {
-        List<T> list = new ArrayList<T>();
-        for (T[] array : twoDArray) {
-            list.addAll(Arrays.asList(array));
-        }
-        return list;
-    }
 }
